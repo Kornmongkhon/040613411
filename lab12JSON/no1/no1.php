ba8c62704d091bcfc3ae2e99938f7045b18d8df9
skyconnor30 <s6404062610391@email.kmutnb.ac.th>
2023-10-23 11:05:34 +0700

    add lab12JSON

---
+++ lab12JSON/no1/no1.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>no1</title>
+    <script>
+        async function getDataFromAPI(){
+            let response = await fetch('https://data.go.th/dataset/296f32c6-8c7e-4a54-ade0-0913d35d3168/resource/d132638d-a243-4829-aed8-10ed4fad917f/download/priv_hos.json');
+            let rawData = await response.text(); //อ่านผลลัพธ์
+            let OBJdata = JSON.parse(rawData);
+            let result = document.getElementById('result');
+
+            for(let i=0;i<OBJdata.features.length;i++){
+                let content = 'ชื่อโรงพยาบาล : '+OBJdata.features[i].properties.name+' '+OBJdata.features[i].properties.num_bed+' เตียง';
+
+                let li = document.createElement('li')
+                
+                li.innerHTML = content;
+                result.appendChild(li);
+            }
+        }
+        getDataFromAPI();
+    </script>
+</head>
+<body>
+    <h1>โรงพยาบาลเอกชน ในกทม.</h1>
+    <ol id="result"></ol>
+</body>
+</html>
